ac219e23bc7c7943487a2be8a23f684719df10d3
Sean Jaeger <seanljaeger@gmail.com>
2023-05-21 17:22:41 +0800

    Cleaned up and wrote comments for index.js

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ require("dotenv").config()
 const express = require("express")
 const { MongoClient, ServerApiVersion } = require("mongodb")
 const cors = require("cors")
+const sanitize = require("mongo-sanitize")
 
-const uri = process.env.MONGODB_URI
+// Throw error if envs aren't set
+if (
+  !process.env.MONGODB_URI ||
+  !process.env.DB_NAME ||
+  !process.env.COLLECTION_NAME
+) {
+  throw new Error("Required environment variables not found.")
+}
 
-const client = new MongoClient(uri, {
+// Initialise client
+const client = new MongoClient(process.env.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -25,38 +34,47 @@ async function run() {
     console.log("Attempting to connect to the database...")
     await client.connect()
     console.log("Successfully connected to the database.")
+
+    // Fetch the specified collection from the specified database
     const collection = client
       .db(process.env.DB_NAME)
       .collection(process.env.COLLECTION_NAME)
 
     app.get("/search", async (req, res) => {
-      const query = req.query.q
-      console.log(query)
+      // Sanitize the user's search query
+      const query = sanitize(req.query.q)
+
+      // If the query is empty or not a string, return an error
       if (!query || typeof query !== "string") {
-        return res.status(400).send("Invalid query string provided")
+        return res.status(400).json({ error: "Invalid query string provided." })
       }
 
-      console.log("working")
       try {
-        console.log("Attempting to fetch items from MongoDB...")
+        // Fetch items from MongoDB that match the query
         const items = await collection
           .find({ product_title: { $regex: query, $options: "i" } })
           .toArray()
-        console.log("Items fetched successfully")
-        console.log(items)
-        res.status(200).send(items)
+
+        res.status(200).json(items)
       } catch (err) {
         console.error("Error while fetching items:", err)
-        res.status(500).send("Error occurred while fetching items")
+        res.status(500).json({ error: "Error occurred while fetching items." })
       }
     })
 
-    app.listen(process.env.PORT || 3000, () => {
-      console.log(`Server started on port ${process.env.PORT || 3000}`)
+    const PORT = process.env.PORT || 3000
+
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`)
     })
   } catch (error) {
     console.error("Failed to connect to the database with error:", error)
+    process.exit(1)
   }
 }
 
-run().catch(console.dir)
+// Run the run function and handle any uncaught errors
+run().catch((error) => {
+  console.error("Uncaught error:", error)
+  process.exit(1)
+})
